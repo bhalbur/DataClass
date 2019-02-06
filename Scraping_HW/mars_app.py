87103aaccaacf9360a2916449289c125878e819a
Brian <brian.t.halbur@gmail.com>
2019-02-05 21:03:22 -0800

    Updates 2-5

--- Scraping_HW/mars_app.py
+++ Scraping_HW/mars_app.py
@@ -1,37 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import datetime
-
+from flask_pymongo import PyMongo
+import pandas as pd
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_mission"
+mongo = PyMongo(app)
+last_update = ''
+
 @app.route("/")
 def main():
-	return render_template("index.html", var_text="Hello and Welcome", var2="It's nice to see you again")
+	news = mongo.db.variables.find({'type':'news'})[0]
+	#featured_image = mongo.db.variables.find({'type':'featured_image'})[0]
+	weather = mongo.db.variables.find({'type':'weather'})[0]
+	plain_facts = mongo.db.variables.find_one({'type':'facts'})['content']
+	facts_df = pd.DataFrame([plain_facts])
+	facts = facts_df.to_html(index=False)
+	hemi = mongo.db.variables.find({'type':'hemisphere'})
+	
+	return render_template("index.html", 
+		last_update = last_update,
+		news = news,
+		featured_image = 'abc',
+		weather = weather,
+		facts = facts,
+		hemi = hemi)
+
 
 @app.route("/scrape")
 def scrape():
-	from mars_mission import setup, news, image, weather, facts, hemi, store
-	setup()
-	news()
-	image()
-	weather()
-	facts()
-	hemi()
-	store()
-	return(f'Fresh Data scraped at {datetime.datetime.now()}')
+	from mars_mission import news, image, weather, facts, hemi
+	#news()
+	#image()
+	#weather()
+	#facts()
+	#hemi()
+	last_update = datetime.datetime.now().strftime("%b %d, %Y at %H:%M")
+
+	return redirect("/test")
 
 
-#@app.route("/<var_input>/<var2_input>")
-#def variables(var_input, var2_input):
-#	return render_template("index.html", var_text=var_input, var2=var2_input)
 
+@app.route("/test1")
+def test1():
+	plain_facts = mongo.db.variables.find_one({'type':'facts'})['content']
+	facts_df = pd.DataFrame([plain_facts])
+	facts = facts_df.to_html(index=False)
 
-#@app.route("/classmates")
-#def classmates():
-#	class_list=['Ryan','Albert','Mark','Adam']
-#	instr_list=['Vishal','Adam','Hussain']
-#	return render_template("classmates.html", 
-#		class_list=class_list, instr_list=instr_list)
+	return facts
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Scraping_HW/mars_mission.py
+++ Scraping_HW/mars_mission.py
@@ -3,71 +3,85 @@ import requests
 import pymongo
 from splinter import Browser
 
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars_mission
+db.variables.drop()
+
 
 def news():
-	url = 'https://mars.nasa.gov/news/'
-	response = requests.get(url, timeout=5)
-	soup = BeautifulSoup(response.text, 'html.parser')
-	header = soup.find('div', class_='content_title')
+	news_url = 'https://mars.nasa.gov/news/'
+	executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+	browser = Browser('chrome', **executable_path, headless=True)
+	browser.visit(news_url)
+	news_soup = BeautifulSoup(browser.html, 'html.parser')
+	print(len(browser.html))
+	header = news_soup.find('div', class_='content_title')
 	print(header.text)
-	body = soup.find('div', class_='rollover_description_inner')
+	body = news_soup.find('div', class_='rollover_description_inner')
 	print(body.text)
 	news_dict = {}
-	news_dict['article_title'] = header.text.strip()
-	news_dict['article_content'] = body.text.strip()
+	news_dict['type'] = 'news'
+	news_dict['title'] = header.text.strip()
+	news_dict['content'] = body.text.strip()
 	print(news_dict)
+	db.variables.insert_one(news_dict)
 
 def image():
 	try:
 		executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-		browser = Browser('chrome', **executable_path, headless=False)
+		browser = Browser('chrome', **executable_path, headless=True)
 		featured_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 		browser.visit(featured_url)
 		browser.click_link_by_partial_text('FULL IMAGE')
 		featured_html = browser.html
 		featured_soup = BeautifulSoup(featured_html, 'html.parser')
 		url_suffix = featured_soup.find('img', class_='fancybox-image').attrs['src']
-		featured_img_url = browser.url + url_suffix
+		featured_img_url = 'https://www.jpl.nasa.gov' + url_suffix
 		print(f'Featured img url: {featured_img_url}')
 		featured_dict = {}
-		featured_dict['title'] = 'Featured_Image'
-		featured_dict['img_url'] = featured_img_url
+		featured_dict['type'] = 'featured_image'
+		featured_dict['content'] = featured_img_url
 		print(featured_dict)
+		db.variables.insert_one(featured_dict)
 	except Exception as e: 
 		print(f'Error finding Featured Image: {e}')
 
 def weather():
 	twitter_url='https://twitter.com/marswxreport?lang=en'
-	twit_resp = requests.get(twitter_url, timeout=5)
+	twit_resp = requests.get(twitter_url, timeout=15)
 	twitter_soup = BeautifulSoup(twit_resp.text, 'html.parser')
 	tweets = twitter_soup.find_all('p', class_='TweetTextSize')
 	for tweet in tweets:
 	    if tweet.text[:3] == 'Sol':
 	        print(tweet.text)
 	        weather_dict = {}
-        	weather_dict['weather'] = tweet.text
+	        weather_dict['type'] = 'weather'
+        	weather_dict['content'] = tweet.text
 	        break
 	    else: next
-
+	db.variables.insert_one(weather_dict)
 
 
 def facts():
 	fact_url = 'https://space-facts.com/mars/'
-	fact_resp = requests.get(fact_url, timeout=5)
+	fact_resp = requests.get(fact_url, timeout=15)
 	fact_soup = BeautifulSoup(fact_resp.text, 'html.parser')
 	facts = fact_soup.find_all('tr')
-	fact_list = []
+	fact_dict = {}
+	fact_subdict = {}
+	fact_dict['type'] = 'facts'
 	for fact in facts:
-	    fact_dict = {}
 	    description, value = fact.text.strip().split(':')
-	    fact_dict[description] = value
-	    fact_list.append(fact_dict)
-	print(fact_list)
+	    fact_subdict[description] = value
+	    fact_dict['content'] = fact_subdict
+	print(fact_dict)
+	db.variables.insert_one(fact_dict)
 
 
 def hemi():
 	executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-	browser = Browser('chrome', **executable_path, headless=False)
+	browser = Browser('chrome', **executable_path, headless=True)
 	hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
 	browser.visit(hemi_url)
 	
@@ -84,15 +98,14 @@ def hemi():
 	    dl_tag = hemi_soup.find('div', class_='downloads')
 	    hemi_img = dl_tag.find('a').attrs['href']
 	    print(hemi_img)
+	    hemi_dict['type'] = 'hemisphere'
 	    hemi_dict['title'] = hemi_title
-	    hemi_dict['img_url'] = hemi_img
+	    hemi_dict['content'] = hemi_img
 	    hemi_list.append(hemi_dict)
 	    browser.back()
 	print(hemi_list)
+	db.variables.insert_many(hemi_list)
 
-def store():
-	return('this line in progress')
-	#store the dicts
 
 
 news()
@@ -100,4 +113,3 @@ image()
 weather()
 facts()
 hemi()
-store()
